9bcf7bbe68b7d6490f62acccf18e46b340338a6a
KAwasthi2889 <kaustubh_awasthi@proton.me>
2025-12-12 21:21:40 +0530

    Optamised Day 4

--- 2025/Day4/main.go
+++ 2025/Day4/main.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+type Pairs struct {
+	a, b int
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -32,40 +36,62 @@ func main() {
 	}
 
 	count := 0
-	type Pairs struct {
-		a, b int
-	}
-	var removables []Pairs // store removable indexes
-	// Corners always have less than 4 neighbours
+	var removables []Pairs // store removable indexes, neighboures of removed rolls
 
-	// starting condition
-	if rolls[0][0] {
-		count++
-	}
-	removables = append(removables, Pairs{0, 0})
-
-	for len(removables) > 0 {
-		// remove every removables
-		for _, pair := range removables {
-			rolls[pair.a][pair.b] = false
-		}
-
-		// empty the removables slice
-		removables = []Pairs{}
-
-		for i, row := range rolls {
-			for j, val := range row {
-				// if the val is true and the neighbours are less than 4
-				if val && check(i, j, rolls) {
-					count++
-					removables = append(removables, Pairs{i, j}) // add removables for next round
-				}
+	for i, row := range rolls {
+		for j, val := range row {
+			// if the val is true and the neighbours are less than 4
+			if val && check(i, j, rolls) {
+				removables = append(removables, Pairs{i, j}) // add removables for next round
 			}
 		}
 	}
 
+	for len(removables) > 0 {
+		// remove rolls
+		for _, r := range removables {
+			rolls[r.a][r.b] = false
+			count++
+		}
+
+		remaining := make(map[Pairs]bool)
+
+		for _, r := range removables {
+			find(r.a, r.b, rolls, &remaining) // find remaining neighbours of removed, check them and add removables
+		}
+
+		removables = nil
+
+		for p := range remaining {
+			removables = append(removables, p)
+		}
+
+	}
+
 	fmt.Println(count)
 
+}
+
+func find(i, j int, rolls [][]bool, remaining *map[Pairs]bool) {
+	directions := [8][2]int{
+		{1, 0},   // up
+		{-1, 0},  // down
+		{0, -1},  // left
+		{0, 1},   // right
+		{1, -1},  // up left
+		{1, 1},   // up right
+		{-1, -1}, // down left
+		{-1, 1},  // down right
+	}
+
+	for _, direction := range directions {
+		a, b := i+direction[0], j+direction[1]
+		if a >= 0 && a < len(rolls) && b >= 0 && b < len(rolls[0]) {
+			if rolls[a][b] && check(a, b, rolls) {
+				(*remaining)[Pairs{a, b}] = true
+			}
+		}
+	}
 }
 
 func check(i, j int, rolls [][]bool) bool {
